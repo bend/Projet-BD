042b30db0d3fd94803cda1cffb4c1412e19d7ae2
Bend <ben.daccache@gmail.com>
2011-04-07 09:41:04 +0200

    started client localisation

---
+++ subscreens/client_localisation.php
@@ -0,0 +1,15 @@
+<?php
+	include("../utils/database_connection.php");
+	//TODO SELECT ONLY CLIENTS
+	$query = "SELECT * FROM Identite";
+	database_connect();
+	$result = database_query($query);
+	while($row = $result->fetch()){
+		echo $row['Rue'];
+		echo $row['Numero'];
+		echo $row['Localite'];
+		echo $row['CodePostal'];
+		echo $row['Pays'];
+	}
+
+?>
