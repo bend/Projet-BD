8a53cdb65f0657a63dcc344a18ff952db8e51855
Bend <ben.daccache@gmail.com>
2011-04-23 18:54:45 +0200

    added home screen with preview of last transactions

--- screens/default_screen.php
+++ screens/default_screen.php
@@ -5,5 +5,60 @@
 	?>
 </div>
 <div id="screen_body">
+	<fieldset>
+		<legend>Welcome</legend>
+			<fieldset>
+				<legend>Last Transactions</legend>
+				<?php
+					include("utils/database_connection.php");
+					$query = "SELECT * FROM Transaction WHERE idTran in (Select idTran from Achat ) ORDER BY IdTran DESC LIMIT 3";
+					database_connect();
+					$res = database_query($query);
+					echo '<fieldset>
+						<legend>Last Purchases</legend>';
+						while($row = $res->fetch()){
+							$id = $row['IdTran'];
+							$total = "SELECT Sum(Prix*Quantite) as somme FROM Composition WHERE idTran='$id'";
+							$res2 = database_query($total);
+							$row2 = $res2->fetch();
+							echo '<fieldset>';
+								echo 'VAT Number: ';
+								echo $row['NumTVA'];
+								echo '<br/>Date: ';
+								echo $row['Date'];
+								echo ' ';
+								echo $row['Heure'];
+								echo '<br/>Total: ';
+								echo $row2['somme'];
+								echo ' EUR';
+							echo '</fieldset>';
+						}
+					echo '</fieldset>';
+					echo '<fieldset>
+						<legend>Last Sales</legend>';
+						$query = "SELECT * FROM Transaction WHERE idTran in (Select idTran from Vente ) ORDER BY IdTran DESC LIMIT 3";
+						$res = database_query($query);
+						while($row = $res->fetch()){
+							$id = $row['IdTran'];
+							$total = "SELECT Sum(Prix*Quantite) as somme FROM Composition WHERE idTran='$id'";
+							$res2 = database_query($total);
+							$row2 = $res2->fetch();
+							echo '<fieldset>';
+								echo 'VAT Number: ';
+								echo $row['NumTVA'];
+								echo '<br/>Date: ';
+								echo $row['Date'];
+								echo ' ';
+								echo $row['Heure'];
+								echo '<br/>Total: ';
+								echo $row2['somme'];
+								echo ' EUR';
+							echo '</fieldset>';
+						}
+					echo '</fieldset>';
+				?>
+
+			</fieldset>
+	</fieldset>
 </div>
 
--- sub_menu/sub_menu_default.php
+++ sub_menu/sub_menu_default.php
@@ -1,5 +1,18 @@
 <div id="sub_menu_title">
-	Home options
+	<div id="date"></div>
 </div>
 <div id="sub_menu_contents">
+	<div id="time">
+		<script type="text/javaScript">var clocksize=150;</script>
+		<script src="http://gheos.net/js/clock.js"></script>
+	</div>	
 </div>
+
+
+<script type="text/javascript">
+	var currentDate = new Date()
+	var day = currentDate.getDate()
+	var month = currentDate.getMonth()
+	var year = currentDate.getFullYear()
+	document.getElementById("date").innerHTML = ( day + "/" + month + "/" + year )
+</script>
