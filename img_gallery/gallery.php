bb10d5e818e53eae1cd25dd0a5d848c87f157cd0
Bend <ben.daccache@gmail.com>
2011-04-23 14:36:30 +0200

    some css change in gallery

--- img_gallery/gallery.php
+++ img_gallery/gallery.php
@@ -1,19 +1,21 @@
-<fieldset>
-<div id="gallery">
+
 <?php
+echo '
+<fieldset>
+<div id="gallery">';
 $dir = '../gallery/';
 $abs_dir = 'gallery/';
    // open specified directory
    $dirHandle = opendir($dir);
    while ($file = readdir($dirHandle)) {
 	   if(!is_dir($file) && (strpos($file, '.jpg') || strpos($file,'.png') ||  strpos($file, '.jpeg') ||  strpos($file, '.gif'))>0) {
-		   echo '<img class="fright" width="100" src="';
+		   echo '<img  width="100" src="';
 		   echo $abs_dir;
 		   echo $file;
 		   echo '"/>';
 		}
    }
    closedir($dirHandle);
+echo '</div>
+</fieldset>';
 ?>
-</div>
-</fieldset>
--- img_gallery/gallery_preview.php
+++ img_gallery/gallery_preview.php
@@ -1,19 +1,20 @@
-<fieldset>
-<div id="gallery">
 <?php
+echo '
+<fieldset>
+<div id="gallery">';
 	$dir = '../gallery/';
 	$abs_dir ='gallery/';
    // open specified directory
    $dirHandle = opendir($dir);
    while ($file = readdir($dirHandle)) {
 	   if(!is_dir($file) && (strpos($file, '.jpg') || strpos($file,'.png') ||  strpos($file, '.jpeg') ||  strpos($file, '.gif'))>0) {
-		   echo '<a href="javascript:;"><img onclick="javascript:choose(this.src);" class="fright" width="100" src="';
+		   echo '<a href="javascript:;"><img onclick="javascript:choose(this.src);" ght" width="100" src="';
 		   echo $abs_dir;
 		   echo $file;
 		   echo '"/></a>';
 		}
    }
    closedir($dirHandle);
+echo '</div>
+</fieldset>';
 ?>
-</div>
-</fieldset>
