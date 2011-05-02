eabe5d3ad9098ee12cebb5373f23240c513a1e28
Bend <ben.daccache@gmail.com>
2011-05-02 18:31:46 +0200

    Fixed warnings display when no warnings in suppliers

--- screens/suppliers_screen.php
+++ screens/suppliers_screen.php
@@ -13,26 +13,28 @@
 			$query= "SELECT SUM(Prix*Quantite) as somme ,Fournisseur.NumTVA FROM Fournisseur NATURAL JOIN Transaction NATURAL JOIN Composition 
 			WHERE Fournisseur.NumTVA=Transaction.NumTVA AND Faillite=1 AND Transaction.IdTran=Composition.IdTran AND Date>CurDate()-30 GROUP BY NumTVA";
 			$res = database_query($query);
-			echo '<div class="warning"> These Suppliers Banckrupted these 30 last days</div>';
-			while($row = $res->fetch()){
-				$id = $row['NumTVA'];
-				$query2 = "SELECT * FROM Identite WHERE NumTVA='$id'";
-				database_connect();
-				$res2 = database_query($query2);
-				$row2 = $res2->fetch();
-				echo '<fieldset>';
-					echo '<a id="zoom_img" href="loads/identity_preview.php?id=';
-						echo $row['NumTVA'];
-						echo '"/>VAT Number:	';
-						echo $row2['NumTVA'];
-						echo '</a><br/>Name:	';
-					echo $row2['Nom'];
-					echo ' ';
-					echo $row2['Prenom'];
-					echo '<br/>Total purchase amount: ';
-					echo $row['somme'];
-					echo '</fieldset>';
-			}
+			if($res->rowCount()>0){
+				echo '<div class="warning"> These Suppliers Banckrupted these 30 last days</div>';
+				while($row = $res->fetch()){
+					$id = $row['NumTVA'];
+					$query2 = "SELECT * FROM Identite WHERE NumTVA='$id'";
+					database_connect();
+					$res2 = database_query($query2);
+					$row2 = $res2->fetch();
+					echo '<fieldset>';
+						echo '<a id="zoom_img" href="loads/identity_preview.php?id=';
+							echo $row['NumTVA'];
+							echo '"/>VAT Number:	';
+							echo $row2['NumTVA'];
+							echo '</a><br/>Name:	';
+						echo $row2['Nom'];
+						echo ' ';
+						echo $row2['Prenom'];
+						echo '<br/>Total purchase amount: ';
+						echo $row['somme'];
+						echo '</fieldset>';
+				}
+			}else echo 'No Warnings';
 			?>
 		</div>
 	</fieldset>
