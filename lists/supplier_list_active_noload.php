ed4280d923916d2edf4c9ade618b58b4a80258e6
Bend <ben.daccache@gmail.com>
2011-05-11 10:36:25 +0200

    added untracked file

---
+++ lists/supplier_list_active_noload.php
@@ -0,0 +1,17 @@
+<?php
+	$query = " SELECT NumTVA FROM Fournisseur WHERE Faillite=0";
+	database_connect();
+	$res = database_query($query);
+	echo '<FORM>';
+	echo '<SELECT name="supplier_list" id="supplier_list"  size="1" >';
+	echo '<option selected>-----</option>';
+	while($row = $res->fetch()) { 
+	 	echo '<option>';	
+		echo $row['NumTVA'];
+		echo '</option>';	
+	}
+	echo '</SELECT>';
+	echo '</FORM>';
+	$res->closeCursor();
+	database_close();
+?>
